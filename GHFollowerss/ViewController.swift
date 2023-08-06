9ee25e0a40804c6547499cfccba70f8d24f2ef39
uyguntursunov <uyguntursunov123@gmail.com>
2023-08-06 22:27:35 +0500

    Initial Commit

---
+++ GHFollowerss/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowerss
+//
+//  Created by Uyg'un Tursunov on 06/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
